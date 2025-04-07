9b6a4d07cedd30b451e4b0f2b73a9afbda431a6d
Pedram Niloofari <xwpedram@yahoo.com>
2025-04-07 18:58:04 +0330

    remove some space

--- Program.cs
+++ Program.cs
@@ -10,14 +10,12 @@ namespace Web4
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             builder.Services.AddControllersWithViews();
             builder.Services.AddHttpClient();
             builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
             var app = builder.Build();
-
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
